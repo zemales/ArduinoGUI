57c6e1243e4d00204889d0dd7c1ea2e9fd319381
zemQQ <daemondhand@gmail.com>
2023-09-15 14:47:53 +0300

    arduino temperture sensor

---
+++ ArduinoGUI/WaveShare_MLX90614.h
@@ -0,0 +1,27 @@
+#ifndef   __MLX90614_H
+#define   __MLX90614_H
+
+#include <Wire.h>
+#include <Arduino.h>
+#include <stdint.h>
+
+#define   MLX90614_ADDRESS		0x00
+#define   MLX90614_ADDR_WRITE	0x00	
+#define   MLX90614_ADDR_READ	0x01
+#define   MLX90614_RAM			0x00
+#define   AMBIENT_TEMP			0x06
+#define   OBJECT_TEMP			0x07
+
+class  WaveShare_MLX90614 {
+public:
+	WaveShare_MLX90614(uint8_t addr = MLX90614_ADDRESS);
+	bool begin();
+	double readObjectTemp(void);
+	double readAmbientTemp(void);
+
+private:
+	uint8_t _addr;
+	float readTemp(uint8_t reg);
+};
+
+#endif 
